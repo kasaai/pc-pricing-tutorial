629c450a7345f139dc45dde0c8949c70d16e5fd1
kevinykuo <kevinykuo@gmail.com>
2018-12-07 19:28:33 -0500

    wrap up data joining to close #7

--- R/data-joining.R
+++ R/data-joining.R
@@ -51,12 +51,12 @@ auto_reg <- read_autoseg("data/Autoseg2012B/auto_reg.csv") %>%
     region = DESCRICAO
   )
 
-auto2_vei <- read_autoseg("data/Autoseg2012B/auto2_vei.csv") %>%
+auto2_vei <- read_autoseg("data/Autoseg2012B/auto2_vei.csv", col_types = "cccc") %>%
   rename(
     code = CODIGO,
-    make_model = DESCRICAO,
-    make = GRUPO,
-    make_code = COD_GRUPO
+    model_details = DESCRICAO,
+    model = GRUPO,
+    vehicle_group_code = COD_GRUPO
   )
 
 auto_sexo <- read_autoseg("data/Autoseg2012B/auto_sexo.csv") %>%
@@ -71,21 +71,51 @@ auto_idade <- read_autoseg("data/Autoseg2012B/auto_idade.csv", col_types = "cc")
     age_range = descricao
   )
 
-auto_cau <- read_autoseg("data/Autoseg2012B/auto_cau.csv", col_types = "cc")
-auto_cob <- read_autoseg("data/Autoseg2012B/auto_cob.csv", col_types = "cc")
-auto2_grupo <- read_autoseg("data/Autoseg2012B/auto2_grupo.csv", col_types = "cc")
+auto_cau <- read_autoseg("data/Autoseg2012B/auto_cau.csv", col_types = "cc") %>%
+  rename(
+    code = CODIGO,
+    cause = CAUSA
+  )
 
-policy_table %>%
+auto2_grupo <- read_autoseg("data/Autoseg2012B/auto2_grupo.csv", col_types = "cc") %>%
+  rename(vehicle_group_code = grpid,
+         vehicle_group_description = descricao
+  )
+
+policy_table_mapped <- policy_table %>%
   left_join(auto_cat, by = c(COD_TARIF = "code")) %>%
   left_join(auto_reg, by = c(REGIAO = "code")) %>%
   left_join(auto2_vei, by = c(COD_MODELO = "code")) %>%
   left_join(auto_sexo, by = c(SEXO = "code")) %>%
-  
   left_join(auto_idade, by = c(IDADE = "code")) %>%
   rename(
     vehicle_year = ANO_MODELO,
-    data_year = ENVIO
+    data_year = ENVIO,
+    exposure = EXPOSICAO1,
+    premium = PREMIO1
   ) %>%
-  select(-COD_TARIF, -REGIAO, -COD_MODELO, -SEXO, -IDADE) %>%
-  mutate_if(is.character, trimws) %>%
-  glimpse()
+  select(
+    # stuff we mapped
+    -COD_TARIF, -REGIAO, -COD_MODELO, -SEXO, -IDADE,
+    # stuff we don't need
+    -EXPOSICAO2, -PREMIO2, -IS_MEDIA,
+  ) %>%
+  rename_at(
+    vars(matches("^(FREQ_SIN|INDENIZ)")),
+    function(x) {
+      str_split(x, "(?=[0-9])") %>%
+        map_chr(function(splitted) {
+          value_type <- if (splitted[[1]] == "FREQ_SIN") "claim_count" else "claim_amount"
+          cause <- switch(
+            splitted[[2]],
+            "1" = "theft",
+            "2" = "collision_partial",
+            "3" = "collision_total_loss",
+            "4" = "fire",
+            "9" = "other"
+          )
+          paste(value_type, cause, sep = "_")
+        })
+    }
+  ) %>%
+  mutate_if(is.character, trimws)
