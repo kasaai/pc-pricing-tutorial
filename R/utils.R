32de5aa9d423597c94e15acd38227a806d0d83bc
kevinykuo <kevinykuo@gmail.com>
2018-12-10 14:43:10 -0500

    package structure

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+#' Read Autoseg File
+#' 
+#' @param file Path to file.
+#' @param ... Additional arguments passed to `readr::read_delim()`.
+#' @export
+read_autoseg <- function(file, ...) {
+  readr::read_delim(
+    file, delim = ";", 
+    locale = locale(decimal_mark = ",", encoding = "ISO-8859-1"), 
+    ...
+  )
+}
--- build_article_preview.R
+++
@@ -1,5 +0,0 @@
-rmarkdown::render(
-  "article/article.Rmd", 
-  output_file = "index.html", 
-  output_dir = "public"
-)
