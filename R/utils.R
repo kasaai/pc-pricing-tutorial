6468476374f9cc4952cd1f897291d5a0edfc25d0
Ryan Thomas <rthomas2003@gmail.com>
2019-02-06 22:55:18 -0500

    Moved download functionality to a function int he pricingtutorial package. Updated DESCRIPTION for new packages.

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,44 @@ read_autoseg <- function(file, ...) {
     ...
   )
 }
+
+download_data <- function(periods, dest_dir) {
+
+  if(!fs::dir_exists(dest_dir)) {
+    
+    fs::dir_create(dest_dir)
+    
+  }
+  
+  url_pattern <- "http://www2.susep.gov.br/redarq.asp?arq=Autoseg{period}%2ezip"
+  
+  download_params <- tibble::tibble(period = periods)
+  
+  download_urls <- glue::glue_data(.x = download_params, url_pattern)
+  
+  purrr::walk(download_urls, download_file, dest_dir = dest_dir)  
+  
+}
+
+download_file <- function(x, dest_dir, keep_zip = FALSE) {
+  
+  dest_file <- stringr::str_extract(URLdecode(x), pattern = "(?<==).+$")
+  full_path <- fs::path(dest_dir, dest_file)
+  exdir_path <-  stringr::str_extract(full_path, pattern = ".*?(?=\\.zip$)")
+  
+  if (fs::file_exists(full_path) || fs::dir_exists(exdir_path)) {
+    data_dir <- stringr::str_extract(dest_file, pattern = ".*?(?=\\.zip$)")
+    message(glue("{data_dir} data already downloaded"))
+    return(NULL)
+  
+  }
+  
+  download.file(x, destfile = full_path)
+  unzip(full_path, exdir = exdir_path)
+  
+  if (!keep_zip) {
+  
+    fs::file_delete(full_path)
+    
+  }
+}
--- analysis/data-fetch.R
+++ analysis/data-fetch.R
@@ -1,46 +1,12 @@
 
 library(tidyverse)
 library(fs)
-library(glue)
+library(pricingtutorial)
 
-url_pattern <- "http://www2.susep.gov.br/redarq.asp?arq=Autoseg{year}{half}%2ezip"
-
-download_params <- tibble(year = c(2013, 2013), half = c("A", "B"))
-
-download_urls <- glue::glue_data(.x = download_params, url_pattern)
 
 download_dir <- path(here::here(), "external_data") 
 
-if(!dir.exists(download_dir)) {
-  
-  dir.create(download_dir)
+time_periods <- c("2012B", "2013A")
 
-} 
+download_data(time_periods, download_dir)
 
-download_file <- function(x, dest_dir, keep_zip = FALSE) {
-  
-  dest_file <- str_extract(URLdecode(x), pattern = "(?<==).+$")
-  full_path <- path(dest_dir, dest_file)
-  exdir_path <-  str_extract(full_path, pattern = ".*?(?=\\.zip$)")
-  
-  if (file_exists(full_path) || dir_exists(exdir_path)) {
-    data_dir <- str_extract(dest_file, pattern = ".*?(?=\\.zip$)")
-    message(glue("{data_dir} data already downloaded"))
-    return(NULL)
-  
-  }
-  
-  download.file(x, destfile = full_path)
-  unzip(full_path, exdir = exdir_path)
-  
-  if (!keep_zip) {
-  
-    file_delete(full_path)
-    
-  }
-}
-
-walk(download_urls, download_file, dest_dir = download_dir)
-
-#list.files(download_dir, pattern = "\\.*zip$", full.names = "TRUE") %>% 
-#  walk(~ unzip(.x, exdir = str_extract(.x, pattern = ".*?(?=\\.zip$)")))
