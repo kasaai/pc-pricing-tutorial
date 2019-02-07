e394275a0f40ccd88de6a59fe182847938536e9d
Ryan Thomas <rthomas2003@gmail.com>
2019-02-06 23:04:12 -0500

    Added init documentation for download functions

--- R/utils.R
+++ R/utils.R
@@ -11,7 +11,16 @@ read_autoseg <- function(file, ...) {
   )
 }
 
-download_data <- function(periods, dest_dir) {
+#' Download AutoSeg Data
+#'
+#' @param periods a character vector of time periods to download 
+#' @param dest_dir a folder to save downloaded data to. Will be created if
+#' folder does not exist 
+#' @param keep_zip if FALSE (default) the downloaded zip files are deleted after
+#' they are uncompressed
+#'
+#' @export
+download_data <- function(periods, dest_dir, keep_zip = FALSE) {
 
   if(!fs::dir_exists(dest_dir)) {
     
@@ -25,10 +34,21 @@ download_data <- function(periods, dest_dir) {
   
   download_urls <- glue::glue_data(.x = download_params, url_pattern)
   
-  purrr::walk(download_urls, download_file, dest_dir = dest_dir)  
+  purrr::walk(download_urls, 
+              download_file, 
+              dest_dir = dest_dir, 
+              keep_zip = keep_zip)  
   
 }
 
+
+#' Download Aut0Seg File from URL
+#'
+#' @param x url to download the file from
+#' @param dest_dir folder to save downloaded file to
+#' @param keep_zip if FALSE (default) the downloaded zip file will be deleted
+#' after it has been uncompressed
+#'
 download_file <- function(x, dest_dir, keep_zip = FALSE) {
   
   dest_file <- stringr::str_extract(URLdecode(x), pattern = "(?<==).+$")
