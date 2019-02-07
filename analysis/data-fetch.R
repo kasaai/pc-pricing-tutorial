eac92448f559087105d809263ab5186a76399ec3
Ryan Thomas <rthomas2003@gmail.com>
2019-02-06 21:32:45 -0500

    Updated download_file to check for presence of download folder before attempting to download again. Also added argument to control if zip file is deleted after unzipping.

--- analysis/data-fetch.R
+++ analysis/data-fetch.R
@@ -1,12 +1,11 @@
 
 library(tidyverse)
 library(fs)
-
-#download_dir <- tempdir()
+library(glue)
 
 url_pattern <- "http://www2.susep.gov.br/redarq.asp?arq=Autoseg{year}{half}%2ezip"
 
-download_params <- tibble(year = 2012, half = c("B"))
+download_params <- tibble(year = c(2013, 2013), half = c("A", "B"))
 
 download_urls <- glue::glue_data(.x = download_params, url_pattern)
 
@@ -16,15 +15,32 @@ if(!dir.exists(download_dir)) {
   
   dir.create(download_dir)
 
-}
+} 
 
-download_file <- function(x, dest_dir) {
+download_file <- function(x, dest_dir, keep_zip = FALSE) {
   
   dest_file <- str_extract(URLdecode(x), pattern = "(?<==).+$")
-  download.file(x, destfile = path(dest_dir, dest_file))
+  full_path <- path(dest_dir, dest_file)
+  exdir_path <-  str_extract(full_path, pattern = ".*?(?=\\.zip$)")
+  
+  if (file_exists(full_path) || dir_exists(exdir_path)) {
+    data_dir <- str_extract(dest_file, pattern = ".*?(?=\\.zip$)")
+    message(glue("{data_dir} data already downloaded"))
+    return(NULL)
+  
+  }
+  
+  download.file(x, destfile = full_path)
+  unzip(full_path, exdir = exdir_path)
+  
+  if (!keep_zip) {
+  
+    file_delete(full_path)
+    
+  }
 }
 
 walk(download_urls, download_file, dest_dir = download_dir)
 
-list.files(download_dir, pattern = "\\.*zip$", full.names = "TRUE") %>% 
-  walk(~ unzip(.x, exdir = str_extract(.x, pattern = ".*?(?=\\.zip$)")))
+#list.files(download_dir, pattern = "\\.*zip$", full.names = "TRUE") %>% 
+#  walk(~ unzip(.x, exdir = str_extract(.x, pattern = ".*?(?=\\.zip$)")))
