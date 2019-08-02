fad8b60b006fadf62c12abc50bd78f1896154ea5
kevinykuo <kevinykuo@gmail.com>
2019-08-02 18:39:25 -0400

    don't use here()

--- analysis/data-fetch.R
+++ analysis/data-fetch.R
@@ -1,10 +1,10 @@
 # Gets data from release
 
-download_dir <- path(here::here(), "external_data") 
+download_dir <- file.path("external_data") 
 piggyback::pb_download("Autoseg2012B.zip", dest = download_dir,
                        overwrite = FALSE)
 
-if (!fs::dir_exists("external_data/Autoseg2012B")) {
+if (!dir.exists("external_data/Autoseg2012B")) {
   unzip(
     file.path(download_dir, "Autoseg2012B.zip"), 
     exdir = file.path(download_dir, "Autoseg2012B")
