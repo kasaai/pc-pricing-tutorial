9b0da765fb0d687dee4cdd57acde8870936565c0
kevinykuo <kevinykuo@gmail.com>
2019-08-02 18:25:58 -0400

    download data

--- analysis/data-fetch.R
+++ analysis/data-fetch.R
@@ -1,11 +1,12 @@
 # Gets data from release
 
 download_dir <- path(here::here(), "external_data") 
-piggyback::pb_download("Autoseg2012B.zip", dest = download_dir)
+piggyback::pb_download("Autoseg2012B.zip", dest = download_dir,
+                       overwrite = FALSE)
 
-unzip(
-  file.path(download_dir, "Autoseg2012B.zip"), 
-  exdir = file.path(download_dir, "Autoseg2012B")
-)
-
-
+if (!fs::dir_exists("external_data/Autoseg2012B")) {
+  unzip(
+    file.path(download_dir, "Autoseg2012B.zip"), 
+    exdir = file.path(download_dir, "Autoseg2012B")
+  )
+}
