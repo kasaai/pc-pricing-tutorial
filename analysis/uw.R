96aaf1a5cd62a03bfc219e1559341ca934691830
kevinykuo <kevinykuo@gmail.com>
2019-08-20 21:46:04 -0400

    fix ml model serialization

--- analysis/uw.R
+++ analysis/uw.R
@@ -21,52 +21,41 @@ rec <- recipe(train_set, ~.) %>%
     average_insured_amount = average_insured_amount / 1000,
     region = fct_explicit_na(region),
     make = fct_explicit_na(make),
-    vehicle_category = fct_explicit_na(vehicle_category),
-    exposure = exposure + 0.5, # see #81
-    loss_per_exposure = claim_amount / exposure
+    vehicle_category = fct_explicit_na(vehicle_category)
   ) %>%
   prep(train_set, strings_as_factors = FALSE, retain = TRUE)
 
+predictors <- c("sex", "age_range", "vehicle_age", "make", "vehicle_category",
+                "region", "average_insured_amount")
 # feature columns don't handle factors well currently
 train_set <- juice(rec) %>% 
   mutate_if(is.factor, as.character) %>% 
-  select(sex, vehicle_age, region, average_insured_amount, make, 
-         loss_per_exposure, exposure)
+  bind_cols(select(train_set, claim_amount, exposure)) %>% 
+  mutate(exposure = exposure + 0.5, # see #81
+         loss_per_exposure = claim_amount / exposure)
 
 # Construct feature spec
 feat_spec <- train_set %>% 
-  feature_spec(loss_per_exposure ~ sex + vehicle_age + region +
-                 average_insured_amount + make) %>% 
+  feature_spec(x = predictors, y = "loss_per_exposure") %>% 
   step_numeric_column(vehicle_age, average_insured_amount, 
                       normalizer_fn = scaler_standard()) %>% 
-  step_categorical_column_with_vocabulary_list(sex, region, make) %>%
-  step_indicator_column(sex) %>%
+  step_categorical_column_with_vocabulary_list(
+    sex, age_range, vehicle_category, region, make
+  ) %>%
+  step_indicator_column(sex, age_range, vehicle_category) %>%
   step_embedding_column(region, make) %>%
-  fit()
-
-# Make a toy model
-inputs <- layer_input_from_dataset(
-  train_set %>% select(-loss_per_exposure, -exposure)
-  )
-
-output <- inputs %>% 
-  layer_dense_features(feat_spec$dense_features()) %>% 
-  layer_dense(10, activation = "relu") %>% 
-  layer_dense(1, activation = "relu")
-
-toy_model <- keras_model(inputs, output)
 
 toy_model <- keras_model_sequential(list(
   layer_dense_features(feature_columns = feat_spec$dense_features()),
-    layer_dense(units = 10, activation = "relu"),
-    layer_dense(units = 1, activation = "relu")
+  layer_dense(units = 10, activation = "relu"),
+  layer_dense(units = 1, activation = "relu")
 ))
 
 toy_model %>% 
   compile(optimizer = optimizer_adam(lr = 0.1), loss = "mse")
 
 history <- toy_model %>% 
-  fit(x = select(train_set, -loss_per_exposure, -exposure), 
+  fit(x = select(train_set, predictors), 
       y = train_set$loss_per_exposure, 
       sample_weight = train_set$exposure,
       batch_size = 10000, epochs = 100)
@@ -79,17 +68,25 @@ tensorflow::tf$keras$models$save_model(toy_model, filepath = "model_artifacts/to
 
 # we'll also want to remove the data frame, see
 #  https://github.com/tidymodels/butcher/issues/132
-clean_rec <- butcher::axe_env(rec, verbose = TRUE)
-saveRDS(clean_rec, "model_artifacts/recipe.rds")
+saveRDS(rec, "model_artifacts/recipe.rds")
 
 # Prediction
+
+library(tensorflow)
+library(dplyr)
+library(keras)
+library(recipes)
+library(forcats)
 
 toy_model_loaded <- tensorflow::tf$keras$models$load_model("model_artifacts/toy_model")
 recipe_loaded <- readRDS("model_artifacts/recipe.rds")
 
-new_data <- rec %>% 
-  bake(training_data[1,]) %>% 
-  select(sex, vehicle_age, region, average_insured_amount, make) %>% 
-  mutate_if(is.factor, as.character)
+incoming <- '{"vehicle_group":["GM CHEVROLET CELTA 1.0"],"vehicle_year":[2005],
+"region":["SC - Oeste"],"sex":["Masculino"],"age_range":["Entre 18 e 25 anos"],
+"vehicle_category":["Passeio nacional"],"average_insured_amount":[18179]}'
 
-predict(toy_model_loaded, new_data)
+new_data <- jsonlite::fromJSON(incoming) %>% 
+  as.data.frame() %>% 
+  bake(recipe_loaded, .) %>% 
+  mutate_if(is.factor, as.character)
+keras:::predict.keras.engine.training.Model(toy_model_loaded, new_data)
