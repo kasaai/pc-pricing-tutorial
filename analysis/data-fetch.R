7ccd757d581d381aee880d250611464e36725288
kevinykuo <kevinykuo@gmail.com>
2019-10-22 20:44:53 -0400

    explicit repo in pb_download()

--- analysis/data-fetch.R
+++ analysis/data-fetch.R
@@ -1,7 +1,9 @@
 # Gets data from release
 
 download_dir <- file.path("external_data") 
-piggyback::pb_download("Autoseg2012B.zip", dest = download_dir,
+piggyback::pb_download("Autoseg2012B.zip", 
+                       repo = "kasaai/pc-pricing-tutorial",
+                       dest = download_dir,
                        overwrite = FALSE)
 
 if (!dir.exists("external_data/Autoseg2012B")) {
